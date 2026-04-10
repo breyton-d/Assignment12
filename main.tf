3ad119027cc9ab6e7149ca766e2b7656a251d2b3
breyton-d <breyton.demille@utahtech.edu>
2026-04-10 11:39:24 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 # ── Key pair (point this at your actual public key file) ──────────────────────
 resource "aws_key_pair" "deployer" {
   key_name   = "finance-server-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCfQn/8LKnxYGwjme9s4ljbZGC0HXcwdHmkavAGpNkQtAFtzwl/XNCPTEoNCiWLTCE46rwKA2Gxb6WhrtEglKRt3wgjN8LhMST+eBRkE5K9NErKR/Y20QBolq6QX076sORxZFSyD7jj6VfG+V7ud10UnSC6n42FaxXJz0/Fju0bhRaDcpqj1Lekc3+2ToMKgkh3cbNHKJE0riJbP7ajSokkkY36SOh+TP5bzQADg95kcZZ8+Z2s4uu6APXPE974jfskbFFR7PChuMlQpIrULm8Ev70F2LQ2UfpvLkm7HJgdCxPxzT+4jHxdXijzHD6a9mXJaPkGYqqWumlQQgXDfplmBqeaR32f2Jp5xXTc+24rXwnuVmyuyhjNOhxEMLO3n2yxsQBJV5FXDnWn5+r0S4bJQGim/qxJSNm1kcT+OfHSiv/ESoqFCWU9RXT1lM3zkjdHGLbWcNyIl9+cn250uwHgTTQxPrf3/vnkn3bKMyXcecpKAL5f5RwBaU+FyjLLIRT2+poPXQtKBhysiUAZwK8Ll2JVhfH0J8qYAM8coJOIj+aFvqhFqE0xNMkQGdXI/0/wu6Afx2tU9BH33Kn60Y52FQoMNqVWnPo6/bPm13UpMRFIl9q1uQ0gAl3rHSMa8U42SuY1vYB6u9EbLlPTC+LQtBqfDy9bjCuEXt0Wf7m2iw== breyt@BreytGram"
 }
 
 # ── Security group: SSH only ──────────────────────────────────────────────────
